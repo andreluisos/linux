994e81172477681fb9dfc73ae7a94836934a7944
André Luís <32070796+andreluisos@users.noreply.github.com>
2023-08-27 13:25:20 -0300

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -9,6 +9,11 @@ sudo dnf remove gnome-terminal-nautilus gnome-terminal
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+gsettings set org.gnome.desktop.interface clock-show-date true
+gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
 # RUN ZSH'S install.sh BEFORE
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -17,11 +22,6 @@ git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM
 sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions zsh-completions zsh-history-substring-search)\nautoload -U compinit \&\& compinit/g' ~/.zshrc 
 sed -i 's/# export PATH=$HOME\/bin:\/usr\/local\/bin:$PATH/export PATH=$HOME\/bin:\/usr\/local\/bin:$PATH/g' ~/.zshrc
 export PATH=$HOME/bin:/usr/local/bin:$PATH
-
-gsettings set org.gnome.desktop.interface clock-show-date true
-gsettings set org.gnome.desktop.interface clock-show-seconds true
-gsettings set org.gnome.desktop.interface clock-show-weekday true
-gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 
 # ENABLE FLATHUB BEFORE
 flatpak install flathub -y com.mattjakeman.ExtensionManager
